dcc759b2db4d7347a95fd50fb9d1c3c77eaaa803
Lucas Jones <lucas.jones@dubitlimited.com>
2012-09-19 12:20:41 +0100

    Added void template

---
+++ Misc/VoidTemplate/VoidTemplate/Core/VoidManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VoidTemplate.Core.Managers;
+
+namespace VoidTemplate.Core
+{
+
+    public enum BotState
+    {
+        Navigation, // Finds mobs
+        Combat, // sends guid to combat
+        Loot, // Loot
+        Rest, // Buffs / Heals / Mana
+        Dead
+    }
+
+    public class VoidManager
+    {
+        private BotState curState;
+        private BotState prvState;
+
+        public VoidManager()
+        {
+            Update();
+        }
+
+        public void Update()
+        {
+            switch (curState)
+            {
+                case BotState.Navigation:
+
+
+                    if (NavigationManager.NearbyMobs().Contains("Bear"))
+                    {
+                        NavigationManager.AttackMob(target);
+                    }
+                    else
+                    {
+                        NavigationManager.WalkToNextWaypoint();
+                    }
+
+                    break;
+
+                case BotState.Combat:
+
+                    CombatManager.DoCombat();            
+
+                    break;
+
+                case BotState.Loot:
+
+                    // loot list
+                    // goto rest
+
+                    break;
+
+                case BotState.Rest:
+                    // heal up
+                    // when ready goto navigation
+                    break;
+
+                case BotState.Dead:
+
+                    // NavigationManager.walkToCorpse();
+                    // if revive then loot
+
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+    }
+}
