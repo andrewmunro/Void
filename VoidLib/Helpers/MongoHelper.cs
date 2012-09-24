ef13ec495ec4000eb080f5d63df5f725bcb485da
andrewmunro <amunro_92@hotmail.com>
2012-09-24 16:20:36 +0100

    Adding mongo helper. Drivers located in Lib.

---
+++ VoidLib/Helpers/MongoHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MongoDB.Driver;
+
+namespace BlackRain.Helpers
+{
+    class MongoHelper
+    {
+        public static MongoServer server;
+        public static MongoDatabase database;
+
+        private void connect()
+        {
+            server = MongoServer.Create("mongodb://50.115.175.42:27020");
+            database = server.GetDatabase("void");
+        }
+    }
+}
